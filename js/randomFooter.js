502ef21e7672aa4e070408b8df502d43e8ff2fd5
vinicius <vnsqzs@gmail.com>
2021-01-07 00:55:44 -0400

    refresh v1.3

--- js/randomFooter.js
+++ js/randomFooter.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ 
     var colors = ['#ff4417','#ff0062','#a5c4be'];
     var new_color = colors[Math.floor(Math.random()*colors.length)];
-    $('footer').css('background-color',new_color);
+    $('.pt-1').css('background-color',new_color);
   });
