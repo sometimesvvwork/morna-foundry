965df31747bfc47aaed3fef66d436bb0dbe65ad7
Vinícius Queiróz <viniciusqueiroz@Viniciuss-MacBook-Air.local>
2024-04-11 22:54:20 -0400

    pushing

--- js/localTime.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    var interval = setInterval(function() {
-    var momentNow = moment();
-    $('#time-part').html(momentNow.format('hh:mm:ss YYYY').toLowerCase());
-    }, 100);
-  });
--- js/randomFooter.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){ 
-    var colors = ['#ff4417','#ff0062'];
-    var new_color = colors[Math.floor(Math.random()*colors.length)];
-    $('.pt-1').css('background-color',new_color);
-  });
